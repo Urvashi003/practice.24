449b871893548997652585726973498df5dc93a7
Urvashi003 <143726853+Urvashi003@users.noreply.github.com>
2024-03-21 23:17:46 +0530

    Add files via upload
    POTD of leetcode and gfg

---
+++ 21.03.24.cpp
@@ -0,0 +1,79 @@
+//leetcode potd
+//Reverse Linked List
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+    
+    while(curr != NULL) {
+        ListNode* nextNode = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = nextNode;
+    }
+    return prev;
+    }
+};
+
+
+//gfg potd
+// ZigZag Tree Traversal
+class Solution{
+    public:
+    //Function to store the zig zag order traversal of tree in a list.
+    vector <int> zigZagTraversal(Node* root)
+    {
+    	vector<vector<int>> traversal;
+    	
+    	queue<Node*> qt;
+    	qt.push(root);
+    
+    	while(!qt.empty())
+    	{
+    	    	vector <int> level;
+    	   
+    	    int n =qt.size();
+    	    for(int i=0;i<n;i++)
+    	    {
+    	         Node* element = qt.front();
+    	         qt.pop();
+    	         level.push_back(element->data);
+    	    
+    	     if(element->left)
+    	    {
+    	        qt.push(element->left);
+    	    }
+    	    if(element->right)
+    	    {
+    	        qt.push(element->right);
+    	    }
+    	    }
+    
+    	    
+    	    traversal.push_back(level);
+    	 
+    	}
+    	
+    	vector<int> ans;
+    	
+    	for(int i=0;i<traversal.size();i++)
+    	{
+    	    if(i%2 == 1)
+    	    {
+    	        reverse(traversal[i].begin(),traversal[i].end());
+    	    }
+    
+         for(auto m:traversal[i] )
+         {
+             ans.push_back(m);
+         }
+    	    
+    	}
+    	
+    	
+    	
+    	return ans;
+    }
+};
